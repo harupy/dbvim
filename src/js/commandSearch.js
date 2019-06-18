b35627dafc41a4673eae40124a34a2afd7948151
harupy <hkawamura0130@gmail.com>
2019-06-18 20:19:41 +0900

    fix comment typo

--- src/js/commandSearch.js
+++ src/js/commandSearch.js
@@ -2,7 +2,7 @@ import customKeyMap from './customKeyMap';
 
 export default (keys, keyMap, context) => {
   const keys_ = keys in customKeyMap ? customKeyMap[keys] : keys;
-  // Some key have mutiple command types
+  // Some key have multiple command types
   const cmds = keyMap.filter(cmd => cmd.keys === keys_);
 
   if (cmds.length === 0) return undefined;
